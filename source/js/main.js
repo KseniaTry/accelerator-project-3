3bd66c6a0b318703113044cb31d483051a344312
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-02-01 23:37:27 +0300

    feat: add styles in hero block + swiper

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,66 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+
+// меню
+
+
+// hero swiper
+const heroSlider = document.querySelector('.hero__swiper');
+
+const heroSwiper = new Swiper(heroSlider, {
+  slideClass: 'hero__slide',
+  modules: [Pagination],
+  slidesPerView: 'auto',
+  loop: true,
+  allowTouchMove: true,
+  pagination: {
+    el: '.hero__pagination',
+    type: 'bullets',
+    bulletClass: 'swiper-pagination-bullet hero__bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active hero__bullet--active',
+  },
+  breakpoints: {
+    1440: {
+      allowTouchMove: false,
+      pagination: {
+        clickable: true,
+      }
+    },
+  }
+});
+
+heroSwiper.init();
+
+const heroPagination = document.querySelector('.hero__pagination.swiper-pagination-bullets.swiper-pagination-horizontal');
+
+// расчет высоты тегов <p></p> на случай добавления еще таких же блоков
+const getSlideTextSummaryHeight = (activeSlide) => {
+  const heroSlideTexts = activeSlide.querySelectorAll('p');
+  var slideTextSummaryHeight = 0;
+
+  heroSlideTexts.forEach((slideText) => {
+    slideTextSummaryHeight += slideText.clientHeight;
+  })
+
+  return slideTextSummaryHeight;
+}
+
+// расчет высоты текстового блока слайда
+const getAllTextContentHeight = (activeSlide) => {
+  const heroSlideTitle = activeSlide.querySelector('h2');
+  const heroDetailsButton = activeSlide.querySelector('.hero__button-wrapper');
+
+  const textSummatyHeight = getSlideTextSummaryHeight(activeSlide);
+  const allTextContentHeight = heroSlideTitle.clientHeight + textSummatyHeight + heroDetailsButton.clientHeight;
+  return allTextContentHeight;
+}
+
+// пересчет размера текстового блока активного слайда при переключении слайда
+heroSwiper.on('slideChange', function () {
+  let activeIndex = heroSwiper.realIndex;
+  const heroActiveSlide = document.querySelector(`[data-swiper-slide-index="${activeIndex}"]`)
+  const textContentHeight = getAllTextContentHeight(heroActiveSlide);
+  heroPagination.style.bottom = `${textContentHeight + 20}px`;
+ });
+
