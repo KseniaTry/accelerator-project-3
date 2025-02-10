242c70241503575c06f140888911c53d814204b3
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-02-10 14:32:21 +0300

    feat: add graphics in ewviews block + styles (mobile and tablet) + tests

--- source/js/main.js
+++ source/js/main.js
@@ -370,3 +370,47 @@ faqItems.forEach((item) => {
     item.classList.toggle('faq__accordion-item--active');
   })
 })
+
+// блок REVIEWS
+const reviewsSlider = document.querySelector('.reviews__swiper');
+
+const reviewsSwiper = new Swiper(reviewsSlider, {
+  slideClass: 'reviews__slide',
+  modules: [Navigation, Scrollbar],
+  navigation: {
+    prevEl: '.reviews__button--prev',
+    nextEl: '.reviews__button--next',
+    clickable: true,
+  },
+  scrollbar: {
+    el: '.reviews__scrollbar',
+    draggable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      allowTouchMove: true, // возможность переключения тачем
+      scrollbar: {
+        enabled: false,
+      },
+    },
+    768: {
+      slidesPerView: 1.276,
+      spaceBetween: 30,
+      allowTouchMove: true,
+      scrollbar: {
+        dragSize: 326,
+      },
+    },
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+      allowTouchMove: false,
+      scrollbar: {
+        dragSize: 394,
+      },
+    },
+  },
+});
+
+programsSwiper.init();
