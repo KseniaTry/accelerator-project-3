14bf7c2d5865d409acf8fb88fefc522dbb2caaf9
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-02-19 23:13:29 +0300

    fix: add close menu clicking on sublist item, refactor code

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,8 +1,9 @@
 const headerNav = document.querySelector('.header__nav');
 const headerButton = document.querySelector('.header__toggle');
 const navItems = document.querySelectorAll('.header__nav-item');
-const navSublistItems = document.querySelectorAll('.header__nav-item--sublist');
+const navItemsWithSublist = document.querySelectorAll('.header__nav-item--sublist');
 const pageBody = document.querySelector('.page-body');
+const navSublistItems = document.querySelectorAll('.header__nav-sublist-item');
 
 const switchMenu = () => {
   headerButton.addEventListener('click', (evt) => {
@@ -14,47 +15,47 @@ const switchMenu = () => {
   });
 };
 
+const closeMenu = () => {
+  headerNav.classList.remove('header__nav--opened');
+  headerButton.classList.remove('header__toggle--opened');
+  pageBody.classList.remove('page-body--window-opened');
+  pageBody.classList.remove('overlay');
+}
+
 const closeMenuOnItemClick = () => {
   navItems.forEach((navItem) => {
     if (!navItem.classList.contains('header__nav-item--sublist')) {
       navItem.addEventListener('click', () => {
-        headerNav.classList.remove('header__nav--opened');
-        headerButton.classList.remove('header__toggle--opened');
-        pageBody.classList.remove('page-body--window-opened');
-        pageBody.classList.remove('overlay');
+        closeMenu();
       });
-    };
+    }
   });
 };
 
-// const closeMenuOnSublistItemClick = () => {
-//     navSublistItems.forEach((sublistItem) => {
-//     sublistItem.addEventListener('click', () => {
-//       headerNav.classList.remove('header__nav--opened');
-//       headerButton.classList.remove('header__toggle--opened');
-//     });
-//   });
-// }
+const closeMenuOnSublistItemClick = () => {
+  navSublistItems.forEach((sublistItem) => {
+    sublistItem.addEventListener('click', () => {
+     closeMenu();
+    });
+  });
+}
 
 const closeMenuOnPageClick = () => {
   document.body.addEventListener("click", (evt) => {
     if (evt.target.closest('.header__nav') == null) {
-      headerNav.classList.remove('header__nav--opened');
-      headerButton.classList.remove('header__toggle--opened');
-      pageBody.classList.remove('page-body--window-opened');
-      pageBody.classList.remove('overlay');
+      closeMenu();
     } else {
       closeMenuOnItemClick();
-      // closeMenuOnSublistItemClick();
+      closeMenuOnSublistItemClick();
     }
   });
 };
 
 const openSublist = () => {
-  navSublistItems.forEach((sublistItem) => {
-    sublistItem.addEventListener('click', () => {
-      sublistItem.classList.toggle('header__nav-item--sublist-opened');
-      const sublist = sublistItem.querySelector('.header__nav-sublist');
+  navItemsWithSublist.forEach((item) => {
+    item.addEventListener('click', () => {
+      item.classList.toggle('header__nav-item--sublist-opened');
+      const sublist = item.querySelector('.header__nav-sublist');
       sublist.classList.toggle('header__nav-sublist--opened');
     })
   })
