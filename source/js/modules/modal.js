2d6e10167355168c9847abdb5512bcace3386e1c
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2025-02-20 15:23:58 +0300

    fix: add closing modal by clicking on page outside the modal

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,11 +1,11 @@
-// модальное окно
 const aboutButton = document.querySelector('.about__details');
 const modal = document.querySelector('.modal__wrapper');
 const closeModalButton = document.querySelector('.modal__close');
 const pageBody = document.querySelector('.page-body');
 
 const openModal = () => {
-  aboutButton.addEventListener('click', () => {
+  aboutButton.addEventListener('click', (evt) => {
+    evt.stopPropagation();
     modal.classList.remove('modal__wrapper--closed');
     window.scrollTo(0, 0);
     pageBody.classList.add('page-body--window-opened');
@@ -14,7 +14,8 @@ const openModal = () => {
 };
 
 const closeModal = () => {
-  closeModalButton.addEventListener('click', () => {
+  closeModalButton.addEventListener('click', (evt) => {
+    evt.stopPropagation();
     modal.classList.add('modal__wrapper--closed');
     pageBody.classList.remove('page-body--window-opened');
     pageBody.classList.remove('overlay');
@@ -23,9 +24,13 @@ const closeModal = () => {
 
 const closeModalOnPageClick = () => {
   document.body.addEventListener('click', (evt) => {
+    evt.stopImmediatePropagation();
     if (evt.target.closest('.modal__form') == null) {
-      closeModal();
-    }
+      modal.classList.add('modal__wrapper--closed');
+    } else {
+      pageBody.classList.add('page-body--window-opened');
+      pageBody.classList.add('overlay');
+    };
   });
 };
 
